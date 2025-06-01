aa61628b5c9815780048fa602f0281f1890a16c9
yangchun2 <yangchun2@shopline.com>
2025-06-01 21:20:34 +0800

    remove telemetry

--- packages/frontend/track/src/mixpanel.ts
+++ packages/frontend/track/src/mixpanel.ts
@@ -10,21 +10,10 @@ type Middleware = (
 ) => Record<string, unknown>;
 
 function createMixpanel() {
-  let mixpanel;
-  if (BUILD_CONFIG.MIXPANEL_TOKEN) {
-    mixpanelBrowser.init(BUILD_CONFIG.MIXPANEL_TOKEN || '', {
-      track_pageview: true,
-      persistence: 'localStorage',
-      api_host: 'https://telemetry.affine.run',
-      ignore_dnt: true,
-    });
-    mixpanel = mixpanelBrowser;
-  } else {
-    mixpanel = new Proxy(
+  let mixpanel = new Proxy(
       function () {} as unknown as OverridedMixpanel,
       createProxyHandler()
     );
-  }
 
   const middlewares = new Set<Middleware>();
 
