fb8779f1e5a6c307f6d33bd8079790e95d9ad022
yangchun2 <yangchun2@shopline.com>
2025-06-02 12:12:33 +0800

    一些默认值修改

---
+++ cp-web-js.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ./packages/frontend/apps/web/dist/js/
+cp runtime.*.js ~/Downloads/js/
+cp i18n.*.js ~/Downloads/js/
+cp i18n-langs.i18n-completenesses.*.js ~/Downloads/js/
+cp npm-rxjs.*.js ~/Downloads/js/
+cp npm-emotion.*.js ~/Downloads/js/
+cp npm-react.*.js ~/Downloads/js/
+cp npm-jotai.*.js ~/Downloads/js/
+cp npm-blocksuite.*.js ~/Downloads/js/
+cp vendor.*.js ~/Downloads/js/
+cp index.*.js ~/Downloads/js/
