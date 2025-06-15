0a0a91bc2acd9279d5f289792a1deab4dd8298bd
yangchun2 <yangchun2@shopline.com>
2025-06-15 15:29:21 +0800

    update version

--- build-web.sh
+++ build-web.sh
@@ -3,10 +3,10 @@ export BUILD_TYPE=stable
 yarn affine @affine/web build
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Mac platform"
-  cd ./packages/frontend/apps/web/dist && sed -i '' 's/\/js\//https:\/\/cdn.kpromise.top\/affine\/js\//g' ./selfhost.html
+  cd ./packages/frontend/apps/web/dist && rm .gitignore && sed -i '' 's/\/js\//https:\/\/cdn.kpromise.top\/affine\/js\//g' ./selfhost.html
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "Linux platform"
-  cd ./packages/frontend/apps/web/dist && sed -i 's/\/js\//https:\/\/cdn.kpromise.top\/affine\/js\//g' ./selfhost.html
+  cd ./packages/frontend/apps/web/dist && rm .gitignore && sed -i 's/\/js\//https:\/\/cdn.kpromise.top\/affine\/js\//g' ./selfhost.html
 else
   echo "Unknown platform please change selfhost.html"
 fi
